8cd9b042f86c1020a06b205856a5c130898530ed
Wave-Sh0t <vsh.2509@gmail.com>
2025-12-17 16:44:20 +0530

    Update console

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello from DEV branch!");
+        Console.WriteLine("Console output logic updated.");
+    }
+}
